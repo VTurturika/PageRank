1d531d4ee3aef2672031d1928fd026e76b06c25f
Victor Turturika <v.turturika@gmail.com>
2017-03-04 00:12:51 +0200

    Add graph UI

--- js/index.js
+++ js/index.js
@@ -1,16 +1,55 @@
+'use strict';
 
-const homeworkWeb = [
-    [1/3, 1/2, 0  ],
-    [1/3, 0,   1/2],
-    [1/3, 1/2, 1/2]
-];
+$(document).ready(() => {
 
-const webFromBook = [
-    [0, 1/2, 0, 0  ],
-    [1/3, 0, 0, 1/2],
-    [1/3, 0, 1, 1/2],
-    [1/3, 1/2, 0, 0]
-];
+    let nextId = 6;
+    let network;
 
-console.log("Simple PageRank:\n\t"  + PageRank.simple(homeworkWeb, 0.001));
-console.log("PageRank with taxation:\n\t"  + PageRank.withTaxation(homeworkWeb, 0.001, 0.8));
+    let data = {
+        nodes: [
+            {id: 1, label: '1'},
+            {id: 2, label: '2'},
+            {id: 3, label: '3'},
+            {id: 4, label: '4'},
+            {id: 5, label: '5'},
+        ],
+        edges: [
+            {from: 1, to: 2, arrows: 'to'},
+            {from: 1, to: 3, arrows: 'to'},
+            {from: 1, to: 5, arrows: 'to'},
+            {from: 2, to: 1, arrows: 'to'},
+            {from: 2, to: 4, arrows: 'to'},
+            {from: 3, to: 1, arrows: 'to'},
+            {from: 3, to: 2, arrows: 'to'},
+            {from: 3, to: 4, arrows: 'to'},
+            {from: 3, to: 5, arrows: 'to'},
+            {from: 4, to: 5, arrows: 'to'},
+            {from: 5, to: 1, arrows: 'to'},
+        ]
+    };
+
+    let container = document.getElementById('network');
+    let options = {
+
+        manipulation: {
+            addNode: function (data, callback) {
+
+                data.id = nextId;
+                data.label = nextId++;
+                callback(data);
+            },
+
+            addEdge: function (data, callback) {
+
+                data.arrows = "to";
+                callback(data);
+            }
+        },
+
+        physics: {solver: 'forceAtlas2Based'}
+    };
+
+    network = new vis.Network(container, data, options);
+
+});
+
